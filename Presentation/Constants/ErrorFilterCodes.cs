ed507cc61e3cef78f6b6f87e8d262d356d215e0f
Eduardo Carvalho <eduardscript@protonmail.com>
2024-01-04 01:21:06 +0000

    Added presentation error mapping

---
+++ Presentation/Constants/ErrorFilterCodes.cs
@@ -0,0 +1,8 @@
+﻿namespace Presentation.Constants;
+
+public static class ErrorFilterCodes
+{
+    public const string InternalServerError = "INTERNAL_SERVER_ERROR";
+
+    public const string ModelValidationError = "MODEL_VALIDATION_ERROR";
+}
--- Presentation/Filters/ErrorFilter.cs
+++ Presentation/Filters/ErrorFilter.cs
@@ -1,10 +1,48 @@
-﻿namespace Presentation.Filters;
+﻿using FluentValidation;
+using Presentation.Constants;
+
+namespace Presentation.Filters;
 
 public class ErrorFilter : IErrorFilter
 {
+    private static readonly string EnvironmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")!;
+
     public IError OnError(IError error)
     {
+        if (EnvironmentName == Environments.Development)
+        {
+            return HandleDevelopmentEnvironmentError(error);
+        }
+
+        if (error.Exception is ValidationException validationException)
+        {
+            return HandleValidationException(error, validationException);
+        }
+
+        return error.WithCode(ErrorFilterCodes.InternalServerError);
+    }
+
+    private static IError HandleDevelopmentEnvironmentError(IError error)
+    {
         return error
-            .WithMessage(error.Message);
+            .WithCode(ErrorFilterCodes.InternalServerError)
+            .WithExtensions(new Dictionary<string, object?>
+            {
+                { "StackTrace", error.Exception?.StackTrace }
+            })
+            .WithMessage(error.Exception?.Message ?? "An error occurred while processing your request.");
+    }
+
+    private static IError HandleValidationException(IError error, ValidationException validationException)
+    {
+        var errorsDictionary = new Dictionary<string, object>
+        {
+            { "Errors", validationException.Errors }
+        };
+
+        return error
+            .WithCode(ErrorFilterCodes.ModelValidationError)
+            .WithExtensions(errorsDictionary)
+            .WithMessage("Validation failed for one or more entity properties. See 'Errors' for more details.");
     }
 }
