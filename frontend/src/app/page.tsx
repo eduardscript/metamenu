777ea4d1d79ccf97b2d2b1c6128363b9814ac18e
Eduardo Carvalho <eduardscript@protonmail.com>
2024-01-15 03:15:08 +0000

    Initializing the struggle with CSS. Because as a backend developer, what's better than wrestling with stylesheets, right?

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,11 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      Just a boilerplate
+
+      I don't know how to do CSS
+      How to work with React even more NEXT JS????
+      But we gonna do it
+    </main>
+  )
+}
