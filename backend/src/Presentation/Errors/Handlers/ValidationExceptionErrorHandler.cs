b24864e6f218b4b17b9b43e8423c2a5ac99455c1
Eduardo Carvalho <eduardscript@protonmail.com>
2024-02-15 02:05:15 +0000

    Improved error handling

--- backend/src/Presentation/Errors/Handlers/ValidationExceptionErrorHandler.cs
+++ backend/src/Presentation/Errors/Handlers/ValidationExceptionErrorHandler.cs
@@ -1,4 +1,5 @@
-﻿using FluentValidation;
+﻿using System.Text.Json;
+using FluentValidation;
 using Presentation.Errors.Common;
 
 namespace Presentation.Errors.Handlers;
@@ -7,9 +8,13 @@ public class ValidationExceptionErrorHandler : IErrorHandler<ValidationException
 {
     public IError HandleError(IError error, ValidationException exception)
     {
+        var errors = exception.Errors.GroupBy(e => e.PropertyName)
+            .ToDictionary(g => g.Key, g => g.Select(e => e.ErrorMessage).ToArray());
+
         return error
             .RemoveExtensions()
             .WithCode(ErrorFilterCodes.ModelValidationError)
+            .SetExtension("errors", JsonSerializer.Serialize(errors))
             .WithMessage(exception.Message);
     }
 }
